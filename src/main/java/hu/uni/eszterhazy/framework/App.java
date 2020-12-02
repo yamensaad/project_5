dc238f3b3ec6daef5de477effe0e0a2371e78c09
Yamen <yamen.saa199@gmail.com>
2020-12-02 01:18:11 +0100

    all files

---
+++ src/main/java/hu/uni/eszterhazy/framework/App.java
@@ -0,0 +1,18 @@
+package hu.uni.eszterhazy.framework;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
